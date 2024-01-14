1cbcfa0ee8d9856ed591ee1ed1e95c85fd932441
Kulpot <kulpotwahu@gmail.com>
2024-01-15 02:14:46 +0800

    ...

--- C++ Bitwise AND and OR/CppScratchPad.cpp
+++ C++ Bitwise AND and OR/CppScratchPad.cpp
@@ -2,16 +2,50 @@
 using std::cout;
 using std::endl;
 
+// ------------------------- C++ Bitwise AND and OR ---------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=qTFUnp_KwHg&list=PLRwVmtr-pp043Ah3GxAqNyoMGyo3_LDNh&index=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // --------------------- C++ Hexadecimal Numbers (0x) ----------------------------------------
 //ref link:https://www.youtube.com/watch?v=XcGIE3LOwLY&list=PLRwVmtr-pp043Ah3GxAqNyoMGyo3_LDNh
 
-void main()
-{
-	//int value = 195;	// decimal - compiler does extrawork for binary convertion
-	//cout << value << endl;
-	//cout << std::hex << value << endl;		// output: C3
-	
-	int value = 0xC3;	// hexadecimal	- you can use hexadecimal value into code
-	cout << value << endl;		//output: 195
-	cout << std::hex << value << endl;		// output: C3
-}
+//void main()
+//{
+//	//int value = 195;	// decimal - compiler does extrawork for binary convertion
+//	//cout << value << endl;
+//	//cout << std::hex << value << endl;		// output: C3
+//	
+//	int value = 0xC3;	// hexadecimal	- you can use hexadecimal value into code
+//	cout << value << endl;		//output: 195
+//	cout << std::hex << value << endl;		// output: C3
+//}
